58547e9d661c7c64207656f5e8099f1387585bfc
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-24 10:51:40 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/br/masmangan/beecrowd/bee1001/Categories.java
@@ -0,0 +1,21 @@
+package br.masmangan.beecrowd.bee1001;
+
+public interface Categories {
+
+	interface setA {
+
+	}
+
+	interface setB {
+
+	}
+
+	interface getSum {
+
+	}
+
+	interface getProduct {
+
+	}
+
+}
