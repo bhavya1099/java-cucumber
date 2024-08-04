cb141cec96f945bc9261bb1826a2623afc4b7eb2
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-08-04 20:45:35 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/br/masmangan/beecrowd/bee1005/Categories.java
@@ -0,0 +1,9 @@
+package br.masmangan.beecrowd.bee1005;
+ public interface Categories{
+    interface setA{}
+    
+    interface roostTestTag1{}
+interface roostTestTag2{}
+   interface setB{}
+ interface getAverage{}
+}
---
+++ src/test/java/br/masmangan/beecrowd/bee1007/Categories.java
@@ -0,0 +1,11 @@
+package br.masmangan.beecrowd.bee1007;
+ public interface Categories{
+    interface setA{}
+    
+    interface roostTestTag1{}
+interface roostTestTag2{}
+   interface setB{}
+ interface setC{}
+ interface setD{}
+ interface getDifference{}
+}
