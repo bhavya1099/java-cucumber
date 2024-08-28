b0096eef7948711e053115e7971a1fb2d45dd465
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-08-28 12:05:46 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4-turbo

--- src/test/java/br/masmangan/beecrowd/bee1007/Categories.java
+++ src/test/java/br/masmangan/beecrowd/bee1007/Categories.java
@@ -1,5 +1,6 @@
 package br.masmangan.beecrowd.bee1007;
- public interface Categories {
+
+public interface Categories {
 
 	interface valid {
 
@@ -25,9 +26,16 @@ package br.masmangan.beecrowd.bee1007;
 
 	}
 
- interface setC{}
- 
- interface setD{}
- 
- interface getDifference{}
+	interface setC {
+
+	}
+
+	interface setD {
+
+	}
+
+	interface getDifference {
+
+	}
+
 }
